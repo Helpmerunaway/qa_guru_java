0274ce490c87b9a7de716bd564bc8de4ffe94fbd
Ruslan <SolovyovRI@yandex.ru>
2025-06-13 16:04:10 +0300

    lection 4 - selenide

---
+++ src/test/java/CssXpathExamples.java
@@ -0,0 +1,25 @@
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CssXpathExamples {
+    void cssXpathExamples(){
+        $("[data-testid=email]").setValue("1");
+        $(by("data-testid", "email")).setValue("1");
+        $("#email").setValue("1");
+        $("[id=email]").setValue("1");
+        $("input#email").setValue("1");
+        $x("//*[@id='email']").setValue("1");
+        $x("//input[@id='email']").setValue("1");
+        $("[name=email]").setValue("1");
+        $(byName("email")).setValue("1");
+        $("[class=email]").setValue("1");
+        $(".email").setValue("1");
+        $(".inputtext").$(".login_form_input_box").setValue("1");
+        $(".inputtext .login_form_input_box").setValue("1");
+
+        $x("//div[text()='Hello, qa.guru!']").click();
+        $(byText("Hello, qa.guru!")).click();
+        $(withText("lo, qa.guru!")).click();
+    }
+}
---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest(){
+        open("/text-box");
+        $("#userName").setValue("Serega Pirat");
+        $("#userEmail").setValue("serega@pirat.com");
+        $("#currentAddress").setValue("Kachalka");
+        $("#permanentAddress").setValue("Thailand");
+        $("#submit").click();
+        $("#output").shouldHave(text("Serega Pirat"), text("serega@pirat.com"), text("Kachalka"), text("Thailand"));
+    }
+
+}
